f2805ca03e426d3110facc25cf024142d67f30b2
Chocoding1 <galmeagi2@naver.com>
2025-11-20 15:52:06 +0900

    Refactor: 메서드명 변경

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,32 +1,32 @@
 import { axiosInstance } from "./axiosInstance.js";
 
-export async function login({ email, password }) {
+export async function loginApi({ email, password }) {
   return axiosInstance.post("/login", { email, password });
 }
 
-export async function sendEmailCode(email) {
+export async function sendEmailCodeApi({ email }) {
   return axiosInstance.post("/emails/send-code", { email });
 }
 
-export async function verifyEmailCode(email, authCode) {
+export async function verifyEmailCodeApi({ email, authCode }) {
   return axiosInstance.post("/emails/verify-code", { email, authCode });
 }
 
-export async function signup({ email, password, name }) {
+export async function signupApi({ email, password, name }) {
   return axiosInstance.post("/members", { email, password, name });
 }
 
-export async function getChatList() {
+export async function getChatListApi() {
   const res = await axiosInstance.get("/chats/conversations");
   return res.data?.data?.conversationDtos || [];
 }
 
-export async function getChatHistory(chatId) {
+export async function getChatHistoryApi(chatId) {
   const res = await axiosInstance.get(`/chats/conversations/${chatId}`);
   return res.data?.data?.messageDtos || [];
 }
 
-export async function sendMessage(chatId, prompt) {
+export async function sendMessageApi(chatId, prompt) {
   if (chatId) {
     const res = await axiosInstance.post(`/chats/${chatId}`, { prompt });
     return res.data;
@@ -36,7 +36,7 @@ export async function sendMessage(chatId, prompt) {
   }
 }
 
-export async function logout(refreshToken) {
+export async function logoutApi(refreshToken) {
   return axios.post("/logout", null, {
     headers: { Authorization: refreshToken },
   });
